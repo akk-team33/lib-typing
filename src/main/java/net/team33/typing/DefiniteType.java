85498ef67a2e16f1d6d64ce2d26c73c2d5079a37
Andi Kluge <andi.kluge@mobi4friends.de>
2018-07-20 11:24:23 +0200

    added JavaDoc to DefiniteType

--- src/main/java/net/team33/typing/DefiniteType.java
+++ src/main/java/net/team33/typing/DefiniteType.java
@@ -8,6 +8,30 @@ import java.util.Optional;
 
 import static java.util.stream.Collectors.joining;
 
+/**
+ * <p>
+ * Represents a fully defined type, possibly based on a generic class.
+ * </p><p>
+ * For example, an instance of {@code DefiniteType<Map<String, List<String>>>}
+ * represents the type {@code Map<String, List<String>>}.
+ * </p><p>
+ * To get an instance of (any) DefiniteType, you first need to create a fully defined derivative of DefiniteType.
+ * Only this can be instantiated directly.
+ * The easiest way to achieve this is to use an anonymous derivation with simultaneous instantiation. Example:
+ * </p><pre>
+ * final DefiniteType&lt;Map&lt;String, List&lt;String&gt;&gt;&gt; stringToStringListMapType
+ *         = new DefiniteType&lt;Map&lt;String, List&lt;String&gt;&gt;&gt;() { };
+ * </pre><p>
+ * If a simple class object already fully defines the type in question,
+ * there is a convenience method to obtain an instance of DefiniteType. Example:
+ * </p><pre>
+ * final DefiniteType&lt;String&gt; stringType
+ *         = DefiniteType.of(String.class);
+ * </pre><p>
+ * <b>Note</b>: This class is defined as an abstract class, but does not define an abstract method
+ * to enforce that a derivative is required for an instantiation.
+ * </p>
+ */
 @SuppressWarnings("AbstractClassWithoutAbstractMethods")
 public abstract class DefiniteType<T> {
 
@@ -18,6 +42,14 @@ public abstract class DefiniteType<T> {
 
     private transient volatile String representation = null;
 
+    private void eg() {
+        final DefiniteType<String> stringType
+                = DefiniteType.of(String.class);
+    }
+
+    /**
+     * @see DefiniteType
+     */
     protected DefiniteType() {
         final ParameterizedType genericSuperclass = (ParameterizedType) getClass().getGenericSuperclass();
         final Variant variant = Variant.of(genericSuperclass.getActualTypeArguments()[0], Parameters.EMPTY);
@@ -35,6 +67,9 @@ public abstract class DefiniteType<T> {
         parameters = Parameters.EMPTY;
     }
 
+    /**
+     * @see DefiniteType
+     */
     public static <T> DefiniteType<T> of(final Class<T> simpleClass) {
         return new DefiniteType<T>(simpleClass) {
         };
