6d01798a81bec6438ffbcae2ecec6cb8e2a182e9
andi <andi.kluge@team33.de>
2020-02-10 15:49:12 +0100

    removed Late & Key, refined some java doc

--- src/main/java/de/team33/libs/typing/v3/Type.java
+++ src/main/java/de/team33/libs/typing/v3/Type.java
@@ -1,5 +1,7 @@
 package de.team33.libs.typing.v3;
 
+import de.team33.libs.provision.v1.LazyMap;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
@@ -13,32 +15,38 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import de.team33.libs.provision.v1.LazyMap;
-
 
 /**
- * <p>
- * Represents a definite type description that can be based on a generic as well as a non-generic class. Examples:
- * </p><ul>
- * <li>an instance of <strong>{@code Type<Map<String, List<String>>>}</strong> represents the type
- * <strong>{@code Map<String, List<String>>}</strong>.</li>
- * <li>an instance of <strong>{@code Type<String>}</strong> represents the type <strong>{@code String}</strong>.</li>
- * </ul><p>
- * To get an instance of Type, you need to create a definite derivative of Type.
- * The easiest way to achieve this is to use an anonymous derivation with simultaneous instantiation. Examples:
- * </p><pre>
- * final Type&lt;Map&lt;String, List&lt;String&gt;&gt;&gt; mapStringToListOfStringType
- *         = new Type&lt;Map&lt;String, List&lt;String&gt;&gt;&gt;() { };
+ * <p>Represents a definite type that can be based on a generic as well as a non-generic class. Examples:</p>
+ * <p>Just as an instance of <b>{@code Class<String>}</b> represents the class <b>{@code String}</b>, an instance of
+ * <b>{@code Type<String>}</b> represents the type <b>{@code String}</b>, with no difference between the <b>class</b>
+ * {@code String} and the <b>type</b> {@code String}.</p>
+ * <p>In addition, an instance of <b>{@code Type<Map<String, List<String>>>}</b> represents the (parameterized) type
+ * <b>{@code Map<String, List<String>>}</b>, while a corresponding instance of <b>{@code Class<...>}</b> is not
+ * possible.</p>
+ * <p>To get an instance of Type, you need to create a definite derivative of Type. Example:</p>
+ * <pre>
+ * public class MapStringToStringListType extends Type&lt;Map&lt;String, List&lt;String&gt;&gt;&gt;() {
+ * }
+ *
+ * final Type&lt;Map&lt;String, List&lt;String&gt;&gt;&gt; mapStringToStringListType =
+ *         new MapStringToStringListType();
+ * </pre>
+ * <p>A more convenient way to achieve this is to use an anonymous derivation with simultaneous instantiation.
+ * Examples:</p>
+ * <pre>
+ * final Type&lt;Map&lt;String, List&lt;String&gt;&gt;&gt; mapStringToStringListType =
+ *         new Type&lt;Map&lt;String, List&lt;String&gt;&gt;&gt;() { };
  * </pre><pre>
- * final Type&lt;String&gt; stringType
- *         = new Type&lt;String&gt;() { };
+ * final Type&lt;String&gt; stringType =
+ *         new Type&lt;String&gt;() { };
  * </pre><p>
- * If, as in the last case, a simple class already fully defines the type concerned, there is a convenience method to
- * get a corresponding Type instance. Example:
+ * If, as in the last case, a simple class already fully defines the type concerned, there is an even more convenient
+ * method to get a corresponding Type instance. Example:
  * </p><pre>
  * final Type&lt;String&gt; stringType
  *         = Type.of(String.class);
- * </pre><p>
+ * </pre>
  *
  * @see #Type()
  * @see #of(Class)
@@ -49,33 +57,14 @@ public abstract class Type<T> {
     private static final Stream<? extends Type<?>> EMPTY = Stream.empty();
     private static final String NOT_DECLARED_IN_THIS = "member (%s) is not declared in the context of type (%s)";
 
-    private static final Key<List<Object>> LIST_VIEW = Type::newListView;
     private static final Key<String> STRING_VIEW = Type::newStringView;
     private static final Key<Integer> HASH_CODE = Type::newHashCode;
+    private static final Key<List<String>> FORMAL_PARAMS = Type::newFormalParameters;
     private static final Key<List<Type<?>>> ACTUAL_PARAMS = Type::newActualParameters;
+    private static final Key<List<Object>> LIST_VIEW = Type::newListView;
 
     private final Stage stage;
-    private final LazyMap<Type<?>> lazy = new LazyMap<>(this);
-
-    private List<Object> newListView() {
-        return Arrays.asList(getUnderlyingClass(), getActualParameters());
-    }
-
-    private String newStringView() {
-        return stage.toString();
-    }
-
-    private Integer newHashCode() {
-        return lazy.get(LIST_VIEW).hashCode();
-    }
-
-    private List<Type<?>> newActualParameters() {
-        return Collections.unmodifiableList(
-                stage.getActualParameters().stream()
-                        .map(Type::of)
-                        .collect(Collectors.toList())
-        );
-    }
+    private final transient LazyMap<Type<?>> lazy = new LazyMap<>(this);
 
     /**
      * Initializes a {@link Type} based on its well-defined derivative.
@@ -93,7 +82,9 @@ public abstract class Type<T> {
     }
 
     /**
-     * Returns a {@link Type} based on a simple {@link Class}.
+     * <p>Returns a {@link Type} based on a simple {@link Class}.</p>
+     * <p>Such a {@link Type} has no {@linkplain #getActualParameters() actual parameters}, but may have
+     * {@linkplain #getFormalParameters() formal parameters}.</p>
      */
     public static <T> Type<T> of(final Class<T> simpleClass) {
         return new Type<T>(ClassVariant.toStage(simpleClass)) {
@@ -103,6 +94,30 @@ public abstract class Type<T> {
     private static Type<?> of(final Stage stage) {
         return new Type(stage) {
         };
+    }
+
+    private List<Object> newListView() {
+        return Arrays.asList(getUnderlyingClass(), getActualParameters());
+    }
+
+    private String newStringView() {
+        return stage.toString();
+    }
+
+    private Integer newHashCode() {
+        return lazy.get(LIST_VIEW).hashCode();
+    }
+
+    private List<String> newFormalParameters() {
+        return stage.getFormalParameters();
+    }
+
+    private List<Type<?>> newActualParameters() {
+        return Collections.unmodifiableList(
+                stage.getActualParameters().stream()
+                     .map(Type::of)
+                     .collect(Collectors.toList())
+        );
     }
 
     /**
@@ -118,7 +133,7 @@ public abstract class Type<T> {
      * @see #getActualParameters()
      */
     public final List<String> getFormalParameters() {
-        return stage.getFormalParameters();
+        return lazy.get(FORMAL_PARAMS);
     }
 
     /**
@@ -157,7 +172,7 @@ public abstract class Type<T> {
      */
     public final Optional<Type<?>> getSuperType() {
         return Optional.ofNullable(getUnderlyingClass().getGenericSuperclass())
-                .map(this::getMemberType);
+                       .map(this::getMemberType);
     }
 
     /**
@@ -168,7 +183,7 @@ public abstract class Type<T> {
      */
     public final Stream<Type<?>> getInterfaces() {
         return Stream.of(getUnderlyingClass().getGenericInterfaces())
-                .map(this::getMemberType);
+                     .map(this::getMemberType);
     }
 
     /**
@@ -245,8 +260,8 @@ public abstract class Type<T> {
                                           final Function<Method, java.lang.reflect.Type[]> toGenericTypes) {
         if (getUnderlyingClass().equals(member.getDeclaringClass())) {
             return Stream.of(toGenericTypes.apply(member))
-                    .map(this::getMemberType)
-                    .collect(Collectors.toList());
+                         .map(this::getMemberType)
+                         .collect(Collectors.toList());
         } else {
             return getSuperTypes()
                     .map(st -> st.nullableTypesOf(member, toGenericTypes))
@@ -282,5 +297,6 @@ public abstract class Type<T> {
         return lazy.get(STRING_VIEW);
     }
 
-    private interface Key<R> extends Function<Type<?>, R> {}
+    private interface Key<R> extends Function<Type<?>, R> {
+    }
 }
