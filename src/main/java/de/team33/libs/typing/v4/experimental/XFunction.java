ee2a7a2742f45aea4edd083a4c8f6da330843d50
KlugeKaindl <akk-gthb@team33.de>
2020-11-03 14:00:24 +0100

    experimental: new: XChoices + test

--- src/main/java/de/team33/libs/typing/v4/experimental/XFunction.java
+++ src/main/java/de/team33/libs/typing/v4/experimental/XFunction.java
@@ -3,8 +3,7 @@ package de.team33.libs.typing.v4.experimental;
 import java.util.function.Function;
 
 @FunctionalInterface
-public interface XFunction<I, R, X extends Exception> extends TFunction<I, R, X> {
+public interface XFunction<I, R, X extends Exception> {
 
-    @Override
     R apply(I input) throws X;
 }
